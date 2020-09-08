91622b3ab3622238c368000bbff1605533721ee6
SSAM <ssam@SSAMs-MacBook-Pro.local>
2020-09-07 19:03:16 -0500

    Added color schemes and dynamic color selection

--- projects.js
+++ projects.js
@@ -4,7 +4,7 @@ let projects = [
     devs: ["C++", "Lua", "SDL2"],
     desc:
       "A 2D Game Engine with scripting and level/scenario generating funtionality.",
-    icon: "car",
+    icon: "cogs",
   },
   {
     name: "Dungeon Monster",
@@ -21,19 +21,46 @@ let projects = [
   },
 ];
 
+let color_schemes = [
+  {
+    background: "#175225",
+    icon: "#25e469",
+  },
+  {
+    background: "#421c56",
+    icon: "#5ac3dc",
+  },
+  {
+    background: "#8a1f14",
+    icon: "#ffbd9c",
+  },
+  {
+    background: "#174672",
+    icon: "#f0ffff",
+  },
+];
+
 let projectsDiv = document.getElementById("projects_div");
 
 // generate html of projects
 projects.forEach((p) => {
   let itemDiv = document.createElement("div");
-  itemDiv.className = "card";
+  itemDiv.className = "card project_card";
   itemDiv.style.width = "18rem";
 
   if (p.icon != null) {
+    // select a random color scheme
+    let colIndex = getRndInteger(0, color_schemes.length);
+    let col = color_schemes[colIndex];
+
     // uses icon
     let iconDiv = document.createElement("div");
+    iconDiv.className = "icon_div";
+    iconDiv.style.backgroundColor = col.background;
+
     let icon = document.createElement("i");
-    icon.className = "fas fa-" + p.icon;
+    icon.className = "fas icons fa-" + p.icon;
+    icon.style.color = col.icon;
     iconDiv.appendChild(icon);
     itemDiv.appendChild(iconDiv);
   } else if (p.img != null) {
@@ -52,3 +79,8 @@ projects.forEach((p) => {
 
   projectsDiv.appendChild(itemDiv);
 });
+
+// functions
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
