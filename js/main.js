93f60178c03e01596634c165b0cba9cf572e60f4
santyarellano <santy.arellano039@gmail.com>
2025-02-21 16:46:41 -0800

    particles now work

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     loadSocialMediaIcons();
+
+    const colors = ['#d04fff', '#59a7ff', '#71f579'];
+    const numParticles = 100;
+    createParticles(document.body, colors, numParticles);
 });
 
 const socialMediaData = [
@@ -7,13 +11,13 @@ const socialMediaData = [
         "Item": "LinkedIn",
         "link": "https://www.linkedin.com/in/s-arellano/",
         "icon": "fa-linkedin-square",
-        "color": "#0000FF"
+        "color": "#ADD8E6" // light blue
     },
     {
         "Item": "Twitter",
         "link": "https://twitter.com/santyarellano",
         "icon": "fa-twitter",
-        "color": "#1DA1F2"
+        "color": "#ADD8E6" // light blue
     }
 ];
 
@@ -30,4 +34,20 @@ function loadSocialMediaIcons() {
         });
         socialMediaContainer.appendChild(iconElement);
     });
+}
+
+function createParticles(container, colors, numParticles) {
+    for (let i = 0; i < numParticles; i++) {
+        const particle = document.createElement('div');
+        particle.classList.add('particle');
+        particle.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+        particle.style.left = `${Math.random() * 100}vw`;
+        particle.style.top = `${Math.random() * 100}vh`;
+        particle.style.width = `${Math.random() * 20 + 10}px`; // random width between 10px and 30px
+        particle.style.height = particle.style.width; // make height equal to width for a square shape
+        particle.style.animationDelay = `${Math.random() * 3}s`; // random delay between 0s and 3s for movement
+        particle.style.animationDuration = `${Math.random() * 3 + 2}s`; // random duration between 2s and 5s
+        particle.style.setProperty('--final-opacity', Math.random().toFixed(2)); // random final opacity between 0 and 1
+        container.appendChild(particle);
+    }
 }
